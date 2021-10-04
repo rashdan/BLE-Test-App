62fdd8fd7c843f302a5aa7cd7a4a76d673b4b388
rashdan <rashdan@natiq.se>
2021-10-04 13:50:49 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, NativeModules, NativeEventEmitter, PermissionsAndroid } from 'react-native';
+import { Platform, NativeModules, NativeEventEmitter, PermissionsAndroid, Alert } from 'react-native';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,12 +7,13 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  FlatList,
   useColorScheme,
   View,
 } from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
 import base64 from 'react-native-base64';
-const transactionId ="moniter";
+const transactionId = "moniter";
 
 export default class App extends Component {
 
@@ -21,7 +22,7 @@ export default class App extends Component {
     this.manager = new BleManager()
     this.state = {
       deviceid: '', serviceUUID: '', characteristicsUUID: '', text1: '', device: {}, makedata: [], showToast: false,
-      notificationReceiving: false
+      notificationReceiving: false, allDevices: [], deviceName: ''
     }
   }
 
@@ -34,34 +35,11 @@ export default class App extends Component {
     }, true);
   }
 
-  // scanAndConnect = () => {
-  //   this.manager.startDeviceScan(null, null, (error, device) => {
-  //     if (error) {
-  //       // Handle error (scanning will be stopped automatically)
-  //       console.log('error in scanning')
-  //       return
-  //     }
-
-  //     // Check if it is a device you are looking for based on advertisement data
-  //     // or other criteria.
-  //     console.log(device, 'Device Name here')
-
-  //     // if (device.name === 'TI BLE Sensor Tag' ||
-  //     //   device.name === 'SensorTag') {
-  //     console.log(device.name, 'inside scanning')
-  //     // Stop scanning as it's not necessary if you are scanning for one device.
-  //     this.manager.stopDeviceScan();
-
-  //     // Proceed with connection.
-  //     // }
-  //   });
-  // }
-
-    componentWillUnmount() {
-      this.manager.cancelTransaction(transactionId)
-      this.manager.stopDeviceScan();
-      this.manager.destroy();
-      delete this.manager;
+  componentWillUnmount() {
+    this.manager.cancelTransaction(transactionId)
+    this.manager.stopDeviceScan();
+    this.manager.destroy();
+    delete this.manager;
   }
 
   async writeMesage(code, message) {
@@ -70,53 +48,55 @@ export default class App extends Component {
     const senddata = base64.encode(message);
     if (device) {
       console.log(this.state.serviceUUID, this.state.deviceid, this.state.characteristicsUUID, 'this is console of IDS in message function')
-      device.writeCharacteristicWithResponseForService(this.state.serviceUUID, this.state.characteristicsUUID, senddata).then((characteristic) => {
+      this.manager.servicesForDevice(this.state.deviceid).then((characteristic) => {
 
         console.log("write response");
         console.log(characteristic);
-        // this.alert(message, "success")
-
-        //Sent message and start receiving data
         console.log("device")
         console.log(this.state.serviceUUID, "device", this.state.characteristicsUUID)
         console.log(this.state.device)
-        let snifferService = null
-        var SERVICE_SNIFFER_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
-        var SNIFFER_VOLTAGE_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+        if(characteristic) {
+          alert('Connected Successfully. You can check connection by Clicking on "IsConnected" Button')
+        } else {
+          alert('Device not Connected')
+        }
+        // let snifferService = null
+        // var SERVICE_SNIFFER_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+        // var SNIFFER_VOLTAGE_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
-        device.services().then(services => {
-          let voltageCharacteristic = null
-          snifferService = services.filter(service => service.uuid === this.state.serviceUUID)[0]
-          snifferService.characteristics().then(characteristics => {
-            console.log("characteristics characteristics")
-            console.log(characteristics)
-            this.setState({ notificationReceiving: true })
-            // voltageCharacteristic is retrieved correctly and data is also seems correct
-            voltageCharacteristic = characteristics.filter(c => c.uuid === characteristics[0].uuid)[0]
-            voltageCharacteristic.monitor((error, c) => {
-              // RECEIVED THE ERROR HERE (voltageCharacteristic.notifiable === true)
-              if (error) {
-                console.log("error in monitering", error)
-                return;
-              }
-              else {
-                // console.log("c",base64.decode(c.value))  
-                const data1 = base64.decode(c.value);
-                var s = data1.split(" ");
-                var s1 = parseInt(s[1]);
-                if (isNaN(s1)) { count++; }
-                else {
-                  if (count == 1) {
-                    this.state.makedata.push(<Text key={moment().valueOf()}>{s[0]} : {s1 / 1000} {"\n"} </Text>);
-                    this.setState({ dateTime: "Data Received at : " + moment().format("MMMM Do, h:mm:ss a"), makedata: this.state.makedata });
-                  }
-                  if (count == 3) { count = 0; this.setState({ makedata: [] }) }
-                }
-              }
-            }, transactionId)
-          }).catch(error => console.log(error))
-        })
-        return
+        // device.services().then(services => {
+        //   let voltageCharacteristic = null
+        //   snifferService = services.filter(service => service.uuid === this.state.serviceUUID)[0]
+        //   snifferService.characteristics().then(characteristics => {
+        //     console.log("characteristics characteristics")
+        //     console.log(characteristics)
+        //     this.setState({ notificationReceiving: true })
+        //     // voltageCharacteristic is retrieved correctly and data is also seems correct
+        //     voltageCharacteristic = characteristics.filter(c => c.uuid === characteristics[0].uuid)[0]
+        //     voltageCharacteristic.monitor((error, c) => {
+        //       // RECEIVED THE ERROR HERE (voltageCharacteristic.notifiable === true)
+        //       if (error) {
+        //         console.log("error in monitering", error)
+        //         return;
+        //       }
+        //       else {
+        //         // console.log("c",base64.decode(c.value))  
+        //         const data1 = base64.decode(c.value);
+        //         var s = data1.split(" ");
+        //         var s1 = parseInt(s[1]);
+        //         if (isNaN(s1)) { count++; }
+        //         else {
+        //           if (count == 1) {
+        //             this.state.makedata.push(<Text key={moment().valueOf()}>{s[0]} : {s1 / 1000} {"\n"} </Text>);
+        //             this.setState({ dateTime: "Data Received at : " + moment().format("MMMM Do, h:mm:ss a"), makedata: this.state.makedata });
+        //           }
+        //           if (count == 3) { count = 0; this.setState({ makedata: [] }) }
+        //         }
+        //       }
+        //     }, transactionId)
+        //   }).catch(error => console.log(error))
+        // })
+        // return
       }).catch((error) => {
         console.log(error, 'this is the console of write message function')
       })
@@ -161,11 +141,15 @@ export default class App extends Component {
   }
 
   async scanAndConnect() {
+    console.log('Scanning all devices!!!!')
     this.setState({ text1: "Scanning..." })
     this.manager.startDeviceScan(null, null, async (error, device) => {
       console.log(device.id, "Scanning...");
+      this.setState({ allDevices: [...this.state.allDevices, device.id] }, () => {
+        console.log(this.state.allDevices, 'this is the console of all Devices')
+      })
       const HeadsetID = device.id
-      this.manager.stopDeviceScan();
+      // this.manager.stopDeviceScan();
       // console.log(/[_]/g.test(device.id), 'console of device:')
       // this.manager.stopDeviceScan();
       if (null) {
@@ -191,23 +175,8 @@ export default class App extends Component {
            // }
            
        });*/
-      console.log(HeadsetID, 'outside connect function')
-      this.manager.connectToDevice(HeadsetID, { autoConnect: true }).then((device) => {
-        // console.log(device, 'this the console of connected device!!!!!')
-        (async () => {
-          const services = await device.discoverAllServicesAndCharacteristics()
-          const characteristic = await this.getServicesAndCharacteristics(services)
-          console.log("characteristic")
-          console.log(characteristic.serviceUUID)
-          console.log("Discovering services and characteristics", characteristic.uuid);
-          this.setState({ deviceid: device.id, serviceUUID: characteristic.serviceUUID, characteristicsUUID: characteristic.uuid, device: device })
-          this.setState({ text1: "Conneted to " + device.name })
-        })();
-        this.setState({ device: device })
-        return device.discoverAllServicesAndCharacteristics()
-      }).catch((error) => {
-        console.log(error, 'this is the console of connection error')
-      })
+      //  ++++++++++++++++
+      // ---------------------------
       // .then((device) => {
       //   // return this.setupNotifications(device)
       // }).then(() => {
@@ -220,15 +189,80 @@ export default class App extends Component {
     });
   }
 
+  ConnectFunc(id) {
+    console.log(id, 'outside connect function')
+    this.manager.connectToDevice(id, { autoConnect: true }).then((device) => {
+      // console.log(device, 'this the console of connected device!!!!!')
+      (async () => {
+        const services = await device.discoverAllServicesAndCharacteristics()
+        console.log(services, 'this is the console of descover services')
+        const characteristic = await this.getServicesAndCharacteristics(services)
+        console.log("characteristic")
+        console.log(characteristic.serviceUUID)
+        console.log("Discovering services and characteristics", characteristic.uuid);
+        if(device.id && characteristic.serviceUUID && characteristic.uuid) {
+          this.setState({ deviceid: device.id, serviceUUID: characteristic.serviceUUID, characteristicsUUID: characteristic.uuid, device: device })
+          this.setState({ text1: "Conneted to " + device.name })
+          this.setState({deviceName: device.name})
+          alert('Now, Device is ready to connect')
+        } else {
+          alert('Problem occur while getting Services')
+        }
+      })();
+      this.setState({ device: device })
+      return device.discoverAllServicesAndCharacteristics()
+    }).catch((error) => {
+      console.log(error, 'this is the console of connection error')
+    })
+  }
+
+  checkDeviceConnection() {
+    const device = this.state.device;
+    this.manager.isDeviceConnected(this.state.deviceid).then((res) => {
+      if(res){
+        alert('You are connected to: ' + this.state.deviceName + ' ' + this.state.deviceid)
+      } else {
+        alert('You are not connected with any device')
+      }
+      console.log(res, 'is device connected')
+    })
+  }
+
+  disconnectDevice() {
+    this.manager.cancelDeviceConnection(this.state.deviceid).then((res) => {
+      alert('Disconnected Successfully')
+      console.log(res, 'Device disconnected successfully')
+    })
+  }
+
   render() {
     return (
-      <SafeAreaView style={styles.backgroundStyle} >
-        <TouchableOpacity onPress={() => this.bluetoothConfig()} style={styles.Button_container}>
-          <Text style={{ fontSize: 22, fontWeight: "600", color: 'white' }}>Bluetooth Button</Text>
-        </TouchableOpacity>
-        <Text style={{marginTop: 100, fontSize: 22, fontWeight: '600'}} onPress={() => this.writeMesage("ACK", "ACK Writted")}>ACK Writted</Text>
-        <Text style={{marginTop: 100, fontSize: 22, fontWeight: '600'}} onPress={() => this.writeMesage("ris 0", "ris 0 Writted")}>ris 0 Writted</Text>
-        <Text style={{marginTop: 100, fontSize: 22, fontWeight: '600'}} onPress={() => this.writeMesage("ris 1", "ris 1 Writted")}>ris 1 Writted</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView>
+          <View style={styles.backgroundStyle}>
+            <TouchableOpacity onPress={() => this.bluetoothConfig()} style={styles.Button_container}>
+              <Text style={{ fontSize: 22, fontWeight: "600", color: 'white' }}>Start Scanning</Text>
+            </TouchableOpacity>
+            {this.state.allDevices && <View style={{ width: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+              {this?.state?.allDevices?.map((item) => {
+                return (
+                  <TouchableOpacity onPress={() => this.ConnectFunc(item)} key={item}>
+                    <Text key={item} style={{ fontSize: 16, fontWeight: '600', marginTop: 20 }}>{item}</Text>
+                  </TouchableOpacity>
+                )
+              })}
+            </View>}
+            <TouchableOpacity style={styles.Button_container} onPress={() => this.writeMesage("ACK", "ACK Writted")}>
+              <Text style={{ fontSize: 22, fontWeight: '600', color: 'white' }}>Connect</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Button_container} onPress={() => this.checkDeviceConnection()}>
+              <Text style={{ fontSize: 22, fontWeight: '600', color: 'white' }}>Is Connected</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Button_container} onPress={() => this.disconnectDevice()}>
+              <Text style={{ fontSize: 22, fontWeight: '600', color: 'white' }}>Disconnect</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BLE-Test-App
+
+Please use these commands for running this app:
+
+1. npm install
+2. pod install in ios folder. 
